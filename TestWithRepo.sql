002b07ea26a4450241a94cb6453ecc06af14dfe6
infosunil <infosunil>
2023-11-17 11:55:53 +0000

    add avg

--- TestWithRepo.sql
+++ TestWithRepo.sql
@@ -1,5 +1,7 @@
 -- Databricks notebook source
 
-select  sum(units_purchased) as units_purchased, state From hive_metastore.default.customers 
+select avg(units_purchased) as units_purchased, state From hive_metastore.default.customers 
 group by state
 order by state desc
+
+
